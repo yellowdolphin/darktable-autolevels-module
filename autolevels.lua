2f4307cfa8e81129ac2c3eba451f0e28f65b25f4
yellowdolphin <marius.wanko@gmail.com>
2025-09-27 17:46:44 +0200

    workaround gui.action issues

--- autolevels.lua
+++ autolevels.lua
@@ -221,28 +221,27 @@ local function add_autolevels_curves()
 
   if not fallback_used then
     -- db.write_timestamp needs to be updated, which is compared with mtime on startup
+    -- Here it is done by gui.action, but this is brittle, may cause race conditions, see below.
+    --dt.print_log("writing xmp files for " .. #processed_images .. " images...")
     local sele = dt.gui.selection(processed_images)
-    dt.print_log("writing xmp files for " .. #sele .. " images...")
-    --dt.gui.action("lib/copy_history/write sidecar files", "", "", 1.000)  -- 0.2 ms / xmp
-    local success, err = pcall(function()
-      dt.gui.action("lib/copy_history/write sidecar files", "", "", 1.000)
-    end)
-    if not success then
-      dt.print_log("Error writing sidecar files: " .. tostring(err))  --nothing
-    end
+    dt.gui.action("lib/copy_history/write sidecar files", "", "", 1.000)  -- 0.2 ms / xmp
+    --dt.gui.action("lib/copy_history/write sidecar files", "")  -- worse
     if #sele < #processed_images then
       dt.print("Don't change selection during processing!")
       dt.print_log(#sele.."/"..#processed_images.." images are still selected, trying again...")
       sele = dt.gui.selection(processed_images)
       dt.gui.action("lib/copy_history/write sidecar files", "", "", 1.000)  -- 0.2 ms / xmp
     end
-    dt.print_log("all xmp files updated")  -- 12 ms in total, but xmp files are not touched, write_timestamp not updated!
-
-    -- select any unprocessed images
-    selected_images = flatten_batches(batches, selected_batches)
-    dt.gui.selection(selected_images)
-    dt.print_log(#processed_images .. "/" .. #images .. " images processed, " .. #selected_images .. " remain selected")
+    --dt.print_log("all xmp files updated")
   end
+
+  -- Select any unprocessed images
+  dt.control.sleep(400)  -- 380+ ms OK, 360 ms causes race condition:
+  selected_images = flatten_batches(batches, selected_batches)
+  dt.gui.selection(selected_images)  -- BUG: prevents dt.gui.action("lib/copy_history/write sidecar files", "", "", 1.000)
+  -- #selected_images is irrelevant, {} gives the same, docs say empty table selects nothing
+  -- gui.action always returns nil, not a status string as docs say
+  dt.print_log(#processed_images .. "/" .. #images .. " images processed, " .. #selected_images .. " remain selected")
 
   lock_status_update = true  --prevent selection-changed hook from overwriting this:
   widgets.status.label = num_added_curves .. _(" curve(s) added")
